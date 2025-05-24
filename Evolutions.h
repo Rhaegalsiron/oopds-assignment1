35d8602ee17e63d3fcc71c929c45729d47f23d02
Herbert Pilo <herbertpilo5019@gmail.com>
2025-05-24 19:54:17 +0800

    Cleaned up definitions in Evolutions.h and GenericRobot.h

--- Evolutions.h
+++ Evolutions.h
@@ -11,6 +11,12 @@ class Robot : public AbstractRobot
 public:
     int robot_type; // make sure this is set to the defined constant of your special bot
     Unit *unit;
+    virtual bool useAbility() { return false; };
+    virtual bool useAbility(Unit *unit) { return false; };
+    virtual bool useAbility(int x, int y) { return false; };
+    virtual bool move(int x, int y) { return false; };
+    virtual bool fire(int x, int y) { return false; };
+    virtual bool look(int x, int y) { return false; };
 };
 
 // All special bots have must have a useAbility function that returns true if the ability is used, and false if the ability cannot be used.
@@ -19,32 +25,19 @@ public:
 class MoveBot : public Robot
 {
 public:
-    bool useAbility(Unit *unit) { return false; };
-    virtual bool useAbility() = 0;             // for StealthBot
-    virtual bool useAbility(int x, int y) = 0; // for JumpBot
-    bool move(int x, int y) { return false; };
-    bool fire(int x, int y) { return false; };
-    bool look(int x, int y) { return false; };
+    virtual bool useAbility() { return false; };            // for StealthBot
+    virtual bool useAbility(int x, int y) { return false; }; // for JumpBot
 };
 
 class ActionBot : public Robot
 {
 public:
-    bool useAbility() { return false; };
-    bool useAbility(Unit *unit) { return false; };
     virtual bool useAbility(int x, int y) = 0; // for LongShotBot and SemiAutoBot
-    bool move(int x, int y) { return false; };
-    bool fire(int x, int y) { return false; };
-    bool look(int x, int y) { return false; };
 };
 
 class SeeingBot : public Robot
 {
 public:
-    bool useAbility(int x, int y) { return false; };
-    virtual bool useAbility() = 0;           // for MapVisionBot
-    virtual bool useAbility(Unit *unit) = 0; // for TrackerBot
-    bool move(int x, int y) { return false; };
-    bool fire(int x, int y) { return false; };
-    bool look(int x, int y) { return false; };
+    virtual bool useAbility() { return false; };           // for MapVisionBot
+    virtual bool useAbility(Unit *unit) { return false; }; // for TrackerBot
 };
--- GenericRobot.h
+++ GenericRobot.h
@@ -5,9 +5,6 @@ class GenericRobot : public Robot
 {
 public:
     GenericRobot(Unit *unit);
-    bool useAbility() { return false; };
-    bool useAbility(int x, int y) { return false; };
-    bool useAbility(Unit *unit) { return false; };
     bool move(int x, int y);
     bool fire(int x, int y);
     bool look(int x, int y);
