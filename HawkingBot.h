6b18123a6a580114a6f769327e9592f1b92391cc
CarrotIsTasty <138109195+CarrotIsTasty@users.noreply.github.com>
2025-05-29 18:37:54 +0800

    Update HawkingBot.h

--- HawkingBot.h
+++ HawkingBot.h
@@ -5,7 +5,7 @@
 class HawkingBot : public MoveBot
 {
     public:
-        int robot_type = BLIND_BOT;
+        int robot_type = HAWKING_BOT;
         HawkingBot(Unit *unit);
         bool useAbility();
         bool useAbility(int x, int y) { return false; }; // its blind so no need value for x and y
