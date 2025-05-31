1b99422cf20456664d526b9b24f6a84667654ec0
Hew Wee Bo <64650160+UnderscorePan@users.noreply.github.com>
2025-05-31 15:26:23 +0800

    Update SemiAutoBot.cpp

--- SemiAutoBot.cpp
+++ SemiAutoBot.cpp
@@ -1,3 +1,14 @@
+/**********|**********|**********|
+Program: SemiAutoBot.cpp
+Course: OOPDS
+Trimester: 2410
+Name: HERBERT PILO ANAK BITA / LOH YONG SHENG / MING SU ANN / HEW WEE BO
+ID: 243UC247H9 / 251UC250GG / 243UC246N0 / 251UC250HB
+Lecture Section: TC4L
+Tutorial Section: T16L / T15L (Loh)
+Email: HERBERT.PILO.ANAK@student.mmu.edu.my / LOH.YONG.SHENG@student.mmu.edu.my / MING.SU.ANN@student.mmu.edu.my / hew.wee.bo@student.mmu.edu.my
+Phone: 013-4221107 / 0146666830 / 0146011335 / 0128803121
+**********|**********|**********/
 #include "SemiAutoBot.h"
 
 SemiAutoBot::SemiAutoBot(Unit *unit)
